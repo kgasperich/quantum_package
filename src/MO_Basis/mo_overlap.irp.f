484f99732e2e209232e0eb0ebd2f5a97cf78754a
Kevin Gasperich <kgasperich@gmail.com>
2018-06-11 15:00:33 -0500

    use ao_to_mo routine

--- src/MO_Basis/mo_overlap.irp.f
+++ src/MO_Basis/mo_overlap.irp.f
@@ -1,33 +1,7 @@
 
 BEGIN_PROVIDER [ double precision, mo_overlap,(mo_tot_num,mo_tot_num)]
   implicit none
-  integer :: i,j,n,l
-  double precision :: f
-  integer :: lmax
-  lmax = (ao_num/4) * 4
-  !$OMP PARALLEL DO SCHEDULE(STATIC) DEFAULT(NONE) &
-  !$OMP  PRIVATE(i,j,n,l) &
-  !$OMP  SHARED(mo_overlap,mo_coef,ao_overlap, &
-  !$OMP    mo_tot_num,ao_num,lmax)
-  do j=1,mo_tot_num
-   do i= 1,mo_tot_num
-    mo_overlap(i,j) = 0.d0
-    do n = 1, lmax,4
-     do l = 1, ao_num
-      mo_overlap(i,j) = mo_overlap(i,j) + mo_coef(l,i) * &
-           ( mo_coef(n  ,j) * ao_overlap(l,n  )  &
-           + mo_coef(n+1,j) * ao_overlap(l,n+1)  &
-           + mo_coef(n+2,j) * ao_overlap(l,n+2)  &
-           + mo_coef(n+3,j) * ao_overlap(l,n+3)  )
-     enddo
-    enddo
-    do n = lmax+1, ao_num
-     do l = 1, ao_num
-      mo_overlap(i,j) = mo_overlap(i,j) + mo_coef(n,j) * mo_coef(l,i) * ao_overlap(l,n)
-     enddo
-    enddo
-   enddo
-  enddo
-  !$OMP END PARALLEL DO
+  call real_ao_to_mo(ao_overlap, size(ao_overlap,1), &
+                     mo_overlap, size(mo_overlap,1))
 END_PROVIDER
 
