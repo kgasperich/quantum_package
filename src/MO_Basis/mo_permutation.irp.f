c61ec2410f7ada8bbc958b3fba1a2ed2897cc047
Kevin Gasperich <kgasperich@gmail.com>
2018-06-11 15:01:04 -0500

    update for complex MOs

--- src/MO_Basis/mo_permutation.irp.f
+++ src/MO_Basis/mo_permutation.irp.f
@@ -2,7 +2,7 @@ program permut_mos
  implicit none
  integer :: mo1,mo2
  integer :: i,j,k,l
- double precision :: mo_coef_tmp(ao_num,2)
+ complex*16 :: mo_coef_tmp(ao_num,2)
  print*,'Which MOs would you like to change ?'
  read(5,*)mo1,mo2
  print*,''
