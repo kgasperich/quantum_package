60d6e8a69b65c19a5101cb64add47f7adc87614b
Kevin Gasperich <kgasperich@gmail.com>
2018-06-12 13:29:49 -0500

    modified huckel for complex MOs

--- plugins/Hartree_Fock/huckel.irp.f
+++ plugins/Hartree_Fock/huckel.irp.f
@@ -17,11 +17,19 @@ subroutine huckel_guess
     do i=1,ao_num
       A(i,j) = c * ao_overlap(i,j) * (ao_mono_elec_integral_diag(i) + ao_mono_elec_integral_diag(j))
     enddo
-    A(j,j) = ao_mono_elec_integral_diag(j) + ao_bi_elec_integral_alpha(j,j)
+    A(j,j) = ao_mono_elec_integral_diag(j) + real(ao_bi_elec_integral_alpha(j,j))
+    if (abs(imag(ao_bi_elec_integral_alpha(j,j))) .gt. 1.0d-10) then
+      stop 'diagonal elements of ao_bi_elec_integral_alpha should be real'
+    endif
   enddo
 
-  Fock_matrix_ao_alpha(1:ao_num,1:ao_num) = A(1:ao_num,1:ao_num)
-  Fock_matrix_ao_beta (1:ao_num,1:ao_num) = A(1:ao_num,1:ao_num)
+!  Fock_matrix_ao_alpha(1:ao_num,1:ao_num) = A(1:ao_num,1:ao_num)
+!  Fock_matrix_ao_beta (1:ao_num,1:ao_num) = A(1:ao_num,1:ao_num)
+  call zlacp2('X', ao_num, ao_num, A, size(A,1), &
+         Fock_matrix_ao_alpha, size(Fock_matrix_ao_alpha,1))
+  call zlacp2('X', ao_num, ao_num, A, size(A,1), &
+         Fock_matrix_ao_beta,  size(Fock_matrix_ao_beta, 1))
+  
 
 !  TOUCH mo_coef
 
