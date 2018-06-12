8b2c35485c7dac588537e26628e631b4d247f8f0
Kevin Gasperich <kgasperich@gmail.com>
2018-06-12 16:59:58 -0500

    started to modify DIIS for complex MOs

--- plugins/Hartree_Fock/DIIS.irp.f
+++ plugins/Hartree_Fock/DIIS.irp.f
@@ -12,66 +12,68 @@ BEGIN_PROVIDER [ double precision, threshold_DIIS_nonzero ]
 
 END_PROVIDER
 
-BEGIN_PROVIDER [double precision, FPS_SPF_Matrix_AO, (AO_num, AO_num)]
+BEGIN_PROVIDER [complex*16, FPS_SPF_Matrix_AO, (AO_num, AO_num)]
   implicit none
   BEGIN_DOC
   !   Commutator FPS - SPF
   END_DOC
-  double precision, allocatable  :: scratch(:,:)
+  complex*16, allocatable  :: scratch(:,:)
+  double precision, allocatable :: rwork(:)
   allocate(                                                          &
+      rwork(2*AO_num*AO_num),                                  &
       scratch(AO_num, AO_num)                                  &
       )
   
   ! Compute FP
   
-  call dgemm('N','N',AO_num,AO_num,AO_num,                           &
-      1.d0,                                                          &
+  call zgemm('N','N',AO_num,AO_num,AO_num,                           &
+      (1.d0,0.d0),                                                   &
       Fock_Matrix_AO,Size(Fock_Matrix_AO,1),                         &
       HF_Density_Matrix_AO,Size(HF_Density_Matrix_AO,1),             &
-      0.d0,                                                          &
+      (0.d0,0.d0),                                                   &
       scratch,Size(scratch,1))
   
   ! Compute FPS
   
-  call dgemm('N','N',AO_num,AO_num,AO_num,                           &
-      1.d0,                                                          &
-      scratch,Size(scratch,1),                                       &
-      AO_Overlap,Size(AO_Overlap,1),                                 &
-      0.d0,                                                          &
-      FPS_SPF_Matrix_AO,Size(FPS_SPF_Matrix_AO,1))
-  
+  call zlacrm(AO_num,AO_num,                                         &
+        scratch,size(scratch,1),                                     &
+        AO_Overlap,size(AO_Overlap,1),                               &
+        FPS_SPF_Matrix_AO,size(FPS_SPF_Matrix_AO,1),rwork)
+
+
   ! Compute SP
   
-  call dgemm('N','N',AO_num,AO_num,AO_num,                           &
-      1.d0,                                                          &
-      AO_Overlap,Size(AO_Overlap,1),                                 &
-      HF_Density_Matrix_AO,Size(HF_Density_Matrix_AO,1),             &
-      0.d0,                                                          &
-      scratch,Size(scratch,1))
+  call zlarcm(AO_num,AO_num,                                         &
+        AO_Overlap,size(AO_Overlap,1),                               &
+        HF_Density_Matrix_AO,size(HF_Density_Matrix_AO,1),           &
+        scratch,size(scratch,1),rwork)
+
   
   ! Compute FPS - SPF
   
-  call dgemm('N','N',AO_num,AO_num,AO_num,                           &
-      -1.d0,                                                         &
+  call zgemm('N','N',AO_num,AO_num,AO_num,                           &
+      (-1.d0,0.d0),                                                  &
       scratch,Size(scratch,1),                                       &
       Fock_Matrix_AO,Size(Fock_Matrix_AO,1),                         &
-      1.d0,                                                          &
+      (1.d0,0.d0),                                                   &
       FPS_SPF_Matrix_AO,Size(FPS_SPF_Matrix_AO,1))
+
+  deallocate(scratch, rwork)
 
 END_PROVIDER
 
-bEGIN_PROVIDER [double precision, FPS_SPF_Matrix_MO, (mo_tot_num, mo_tot_num)]
+bEGIN_PROVIDER [complex*16, FPS_SPF_Matrix_MO, (mo_tot_num, mo_tot_num)]
   implicit none
   begin_doc 
 !   Commutator FPS - SPF in MO basis
   end_doc
-  call ao_to_mo(FPS_SPF_Matrix_AO, size(FPS_SPF_Matrix_AO,1), &
+  call complex_ao_to_mo(FPS_SPF_Matrix_AO, size(FPS_SPF_Matrix_AO,1), &
      FPS_SPF_Matrix_MO, size(FPS_SPF_Matrix_MO,1))
 END_PROVIDER
 
 
  BEGIN_PROVIDER [ double precision, eigenvalues_Fock_matrix_AO, (AO_num) ]
-&BEGIN_PROVIDER [ double precision, eigenvectors_Fock_matrix_AO, (AO_num,AO_num) ]
+&BEGIN_PROVIDER [ complex*16, eigenvectors_Fock_matrix_AO, (AO_num,AO_num) ]
 
    BEGIN_DOC
    ! Eigenvalues and eigenvectors of the Fock matrix over the AO basis
@@ -79,14 +81,17 @@ END_PROVIDER
 
    implicit none
    
-   double precision, allocatable  :: scratch(:,:),work(:),Xt(:,:)
-   integer                        :: lwork,info
+   double precision, allocatable  :: work(:),Xt(:,:)
+   integer                        :: lwork,info,lrwork
    integer                        :: i,j
+
+   double precision, allocatable :: rwork(:)
+   complex*16, allocatable       :: scratch(:,:)
    
-   lwork = 3*AO_num - 1
+   lrwork = 2*ao_num*ao_num
    allocate(                                                         &
        scratch(AO_num,AO_num),                                 &
-       work(lwork),                                                  &
+       rwork(lrwork),                                                  &
        Xt(AO_num,AO_num)                                             &
        )
  
@@ -100,27 +105,41 @@ END_PROVIDER
 
 ! Calculate Fock matrix in orthogonal basis: F' = Xt.F.X
 
-  call dgemm('N','N',AO_num,AO_num,AO_num,     &
-       1.d0,                                   &
-       Fock_matrix_AO,size(Fock_matrix_AO,1),  &
-       S_half_inv,size(S_half_inv,1),        &
-       0.d0,                                   &
-       eigenvectors_Fock_matrix_AO,size(eigenvectors_Fock_matrix_AO,1))       
+  call zlacrm(ao_num,ao_num, &
+         Fock_matrix_AO,size(Fock_matrix_AO,1), &
+         S_half_inv,size(S_half_inv,1),        &
+         eigenvectors_Fock_matrix_AO,size(eigenvectors_Fock_matrix_AO,1), &
+         rwork)
 
-  call dgemm('N','N',AO_num,AO_num,AO_num,                              &
-       1.d0,                                                            &
-       Xt,size(Xt,1),                                                   &
-       eigenvectors_Fock_matrix_AO,size(eigenvectors_Fock_matrix_AO,1), &
-       0.d0,                                                            &
-       scratch,size(scratch,1))
+  call zlarcm(ao_num,ao_num, &
+         Xt,size(Xt,1), &
+         eigenvectors_Fock_matrix_AO,size(eigenvectors_Fock_matrix_AO,1), &
+         scratch,size(scratch,1), &
+         rwork)
      
 ! Diagonalize F' to obtain eigenvectors in orthogonal basis C' and eigenvalues
-  
-   call dsyev('V','U',AO_num,       &
+ 
+   deallocate(rwork)
+   lrwork = 3*ao_num - 2
+   allocate( rwork(lrwork), work(1) )
+   lwork = -1
+
+   call zheev('V','U',AO_num,       &
         scratch,size(scratch,1),    &
         eigenvalues_Fock_matrix_AO, &
-        work,lwork,info)
- 
+        work,lwork,rwork,info)
+
+   lwork = work(1)
+   deallocate(work)
+   allocate(work(lwork))
+
+   call zheev('V','U',AO_num,       &
+        scratch,size(scratch,1),    &
+        eigenvalues_Fock_matrix_AO, &
+        work,lwork,rwork,info)
+
+   deallocate(work,rwork)
+
    if(info /= 0) then
      print *,  irp_here//' failed : ', info
      stop 1
@@ -128,12 +147,14 @@ END_PROVIDER
 
 ! Back-transform eigenvectors: C =X.C'
 
-  call dgemm('N','N',AO_num,AO_num,AO_num,     &
-       1.d0,                                   &
+  lrwork = 2*ao_num*ao_num
+  allocate(rwork(lrwork))
+  call zlarcm(ao_num,ao_num,     &
        S_half_inv,size(S_half_inv,1),        &
        scratch,size(scratch,1),                &
-       0.d0,                                   &
-       eigenvectors_Fock_matrix_AO,size(eigenvectors_Fock_matrix_AO,1))       
+       eigenvectors_Fock_matrix_AO,size(eigenvectors_Fock_matrix_AO,1), &
+       rwork)
+  deallocate(rwork)
    
 END_PROVIDER
 
