7e92f75074b147a9f5b78b87e186303d7e1bbcd8
Kevin Gasperich <kgasperich@gmail.com>
2018-06-21 15:47:24 -0500

    utils (Determinants) complex

--- src/Determinants/utils.irp.f
+++ src/Determinants/utils.irp.f
@@ -1,11 +1,11 @@
-BEGIN_PROVIDER [ double precision, H_matrix_all_dets,(N_det,N_det) ]
+BEGIN_PROVIDER [ complex*16, H_matrix_all_dets,(N_det,N_det) ]
   use bitmasks
  implicit none
  BEGIN_DOC
  ! H matrix on the basis of the slater determinants defined by psi_det
  END_DOC
  integer :: i,j,k
- double precision :: hij
+ complex*16 :: hij
  integer :: degree(N_det),idx(0:N_det)
  call  i_H_j(psi_det(1,1,1),psi_det(1,1,1),N_int,hij)
  !$OMP PARALLEL DO SCHEDULE(GUIDED) DEFAULT(NONE) PRIVATE(i,j,hij,degree,idx,k) &
@@ -14,7 +14,7 @@ BEGIN_PROVIDER [ double precision, H_matrix_all_dets,(N_det,N_det) ]
    do j = i, N_det
    call  i_H_j(psi_det(1,1,i),psi_det(1,1,j),N_int,hij)
    H_matrix_all_dets(i,j) = hij
-   H_matrix_all_dets(j,i) = hij
+   H_matrix_all_dets(j,i) = conjg(hij)
   enddo
  enddo
  !$OMP END PARALLEL DO
