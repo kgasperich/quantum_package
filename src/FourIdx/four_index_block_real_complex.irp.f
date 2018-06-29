5bf49120774a01fa276623c2828a74a0f72ee9ce
Kevin Gasperich <kgasperich@gmail.com>
2018-06-29 18:38:36 -0500

    more complex transform things

--- src/FourIdx/four_index_block_real_complex.irp.f
+++ src/FourIdx/four_index_block_real_complex.irp.f
@@ -29,10 +29,10 @@ subroutine four_index_transform_block(map_a,map_c,matrix_B,LDB,            &
   integer                        :: a, b, c, d
   double precision, external     :: get_ao_bielec_integral
   integer*8                      :: ii
-  integer(key_kind)              :: idx
+  integer(key_kind)              :: idx,keytmp1,keytmp2
   real(integral_kind)            :: tmp
-  integer(key_kind), allocatable :: key(:)
-  real(integral_kind), allocatable :: value(:)
+  integer(key_kind), allocatable :: key1(:),key2(:)
+  real(integral_kind), allocatable :: value1(:),value2(:)
   integer*8, allocatable         :: l_pointer(:)
   integer                        :: ac,bd
   !idx2_tri
@@ -146,52 +146,64 @@ subroutine four_index_transform_block(map_a,map_c,matrix_B,LDB,            &
         !$OMP  a_start,a_end,b_start,b_end,c_start,c_end,d_start,d_end,&
         !$OMP  i_start,i_end,j_start,j_end,k_start,k_end,l_start_block,l_end_block,&
         !$OMP  i_min,i_max,j_min,j_max,k_min,k_max,l_min,l_max,      &
-        !$OMP  map_c,matrix_B,l_pointer)                             &
-        !$OMP  PRIVATE(key,value,T,U,V,i,j,k,l,idx,ik,ll,            &
-        !$OMP  a,b,c,d,tmp,T2d,V2d,ii,p,q)
-    allocate( key(i_max*j_max*k_max), value(i_max*j_max*k_max) )
+        !$OMP  map_c,matrix_B,l_pointer)               &
+        !$OMP  PRIVATE(key1,value1,key2,value2,T,U,V,i,j,k,l,idx,ik,ll, &
+        !$OMP  keytmp1,keytmp2,  &
+        !$OMP  a,b,c,d,tmp,T2d,V2d,ii,p,q,amax)
+    allocate( key1(i_max*j_max*k_max), value1(i_max*j_max*k_max), &
+              key2(i_max*j_max*k_max), value2(i_max*j_max*k_max) )
     allocate( U(a_start:a_end, c_start:c_end, b_start:b_end) )
 
 
 
     allocate( T2d((i_end-i_start+1)*(k_end-k_start+2)/2, j_start:j_end), &
-              V2d((i_end-i_start+1)*(k_end-k_start+2)/2, b_start:b_end), &
+              V2d(b_start:b_end, (i_end-i_start+1)*(k_end-k_start+2)/2), &
               V(i_start:i_end, k_start:k_end), &
               T(k_start:k_end, a_start:a_end))
 
 
     !$OMP DO SCHEDULE(dynamic)
     do d=d_start,d_end
-      U = 0.d0
+      U = (0.d0,0.d0)
       do l=l_start_block,l_end_block
-        if (dabs(matrix_B(l,d)) < 1.d-10) then
+        if (cdabs(matrix_B(l,d)) < 1.d-10) then
           cycle
         endif
         
         ii=l_pointer(l)
         do j=j_start,j_end
           !DIR$ VECTOR NONTEMPORAL
-          T2d(:,j) = 0.d0
+          T2d(:,j) = (0.d0,0.d0)
           !DIR$ IVDEP
           do while (j == a_array_j(ii))
-            T2d(a_array_ik(ii),j) = a_array_value(ii)
+            T2d(a_array_ik(ii),j) = dcmplx(a_array_value(ii),0.d0)
             ii = ii + 1_8
           enddo
         enddo ! j
 
-        call DGEMM('N','N', ishft( (i_end-i_start+1)*(i_end-i_start+2), -1),&
+        call ZGEMM('C','T', &
             (b_end-b_start+1),                                             &
-            (j_end-j_start+1), 1.d0,                                   &
+            ishft( (i_end-i_start+1)*(i_end-i_start+2), -1),&
+            (j_end-j_start+1), (1.d0,0.d0)                               &
+            matrix_B(j_start,b_start), size(matrix_B,1),          &
             T2d(1,j_start), size(T2d,1),                               &
-            matrix_B(j_start,b_start), size(matrix_B,1),0.d0,          &
-            V2d(1,b_start), size(V2d,1) )
+            (0.d0,0.d0), &
+            V2d(b_start,1), size(V2d,1) )
+!        call DGEMM('N','N', ishft( (i_end-i_start+1)*(i_end-i_start+2), -1),&
+!            (b_end-b_start+1),                                             &
+!            (j_end-j_start+1), 1.d0,                                   &
+!            T2d(1,j_start), size(T2d,1),                               &
+!            matrix_B(j_start,b_start), size(matrix_B,1),0.d0,          &
+!            V2d(1,b_start), size(V2d,1) )
 
+! V2d is unrolled the wrong way now 
+! (can't take conjugate of B without transposing in ZGEMM, so needed to also transpose T2d and V2d)
         do b=b_start,b_end
           ik = 0
           do k=k_start,k_end
             do i=i_start,k
               ik = ik+1
-              V(i,k) = V2d(ik,b)
+              V(i,k) = V2d(b,ik)
             enddo ! i
           enddo ! k
 
@@ -244,28 +256,61 @@ subroutine four_index_transform_block(map_a,map_c,matrix_B,LDB,            &
 
       idx = 0_8
 
-      integer :: p, q
+      ! loop over unique 4-fold (including only one of each pair of complex conjugates)
+      ! required conditions are:
+      ! b <= d
+      ! ac <= bd (where ac and bd are triangular compound indices)
+      ! .not.((b==d) .and. (a>c))
+      !
+      ! old method of updating p and q (ac and bd) on each iteration would work with minor modification
+      ! if a>c, then p+=1 no longer works (must use p+=(a-1) instead)
+      !
+      ! d=1,n
+      !   b=1,d
+      !     c=1,d
+      !       a=1,d
+      !         if ((b==d) .and. (a>c)) exit
+      !         if (ac > bd) exit 
+      !
+      integer :: p, q, amax
       do b=b_start,d
-        q = b+ishft(d*d-d,-1)
+!        q = b+ishft(d*d-d,-1)
         do c=c_start,c_end
-          p = a_start+ishft(c*c-c,-1)
-          do a=a_start,min(b,c)
+!          p = a_start+ishft(c*c-c,-1)
+          ! this is probably not the best way to handle these loops
+          if (b.eq.d) then
+            amax=c
+          else if (c.le.b) then
+            amax=d
+          else if (c.lt.d) then
+            amax=d-1
+          else
+            amax=b
+          endif
+          do a=a_start,amax
             if (dabs(U(a,c,b)) < 1.d-15) then
               cycle
             endif
-            if ((a==b).and.(p>q)) cycle
-            p = p+1
             idx = idx+1_8
-            call bielec_integrals_index(a,b,c,d,key(idx))
-!print *,  int(key(idx),4), int(a,2),int(b,2),int(c,2),int(d,2), p, q
-            value(idx) = U(a,c,b)
+            call bielec_integrals_index_2fold(a,b,c,d,keytmp1)
+            call bielec_integrals_index_2fold(c,d,a,b,keytmp2)
+            key1(idx) = min(keytmp1,keytmp2)
+            key2(idx) = max(keytmp1,keytmp2)
+            
+            value1(idx) = real(U(a,c,b))
+            if (keytmp1.lt.keytmp2) then
+              value2(idx) = imag(U(a,c,b))
+            else
+              value2(idx) = -1.d0 * imag(U(a,c,b))
+            endif
           enddo ! a
         enddo ! c
       enddo ! b
 
 
       !$OMP CRITICAL
-      call map_update(map_c, key, value, idx,1.d-15) 
+      call map_update(map_c, key1, value1, idx,1.d-15) 
+      call map_update(map_c, key2, value2, idx,1.d-15) 
       !$OMP END CRITICAL
 
   !WRITE OUTPUT
