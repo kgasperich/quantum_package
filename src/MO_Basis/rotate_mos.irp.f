1e6e37502192a4a132474849b1cc4ef43f4ebf81
Kevin Gasperich <kgasperich@gmail.com>
2018-07-06 14:26:16 -0500

    rotate_mos

---
+++ src/MO_Basis/rotate_mos.irp.f
@@ -0,0 +1,21 @@
+program rotate_mos
+  implicit none
+  integer :: i,j
+  double precision :: theta1,costheta,sintheta
+  double precision :: x1,y1,x2,y2
+  print *,  'theta?'
+  read(*,*) theta1
+  costheta = dcos(theta1)
+  sintheta = dsin(theta1)
+  do i=1,ao_num
+    do j=1,mo_num
+      x1 = real(mo_coef(i,j))
+      y1 = imag(mo_coef(i,j))
+      x2 = costheta*x1 - sintheta*y1
+      y2 = sintheta*x1 + costheta*y1
+      mo_coef(i,j) = dcmplx(x2,y2)
+    enddo
+  enddo
+  call save_mos
+
+end
