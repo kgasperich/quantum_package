25b71e7487e86dc471d5cd9fc40100bc6a42a1a1
Kevin Gasperich <kgasperich@gmail.com>
2018-07-09 14:35:29 -0500

    fixed pt2 equations

--- plugins/Perturbation/pt2_equations.irp.f
+++ plugins/Perturbation/pt2_equations.irp.f
@@ -10,7 +10,7 @@ subroutine pt2_epstein_nesbet ($arguments)
   !
   ! for the various N_st states.
   !
-  ! c_pert(i) = <psi(i)|H|det_pert>/( E(i) - <det_pert|H|det_pert> )
+  ! c_pert(i) = <det_pert|H|psi(i)>/( E(i) - <det_pert|H|det_pert> )
   !
   ! e_2_pert(i) = <psi(i)|H|det_pert>^2/( E(i) - <det_pert|H|det_pert> )
   !
@@ -18,13 +18,13 @@ subroutine pt2_epstein_nesbet ($arguments)
   
   integer                        :: i,j
   double precision               :: diag_H_mat_elem_fock, h
-  double precision               :: i_H_psi_array(N_st)
+  complex*16               :: i_H_psi_array(N_st)
   PROVIDE  selection_criterion
 
   ASSERT (Nint == N_int)
   ASSERT (Nint > 0)
   !call i_H_psi(det_pert,psi_selectors,psi_selectors_coef,Nint,N_det_selectors,psi_selectors_size,N_st,i_H_psi_array)
-  call i_H_psi_minilist(det_pert,minilist,idx_minilist,N_minilist,psi_selectors_coef,Nint,N_minilist,psi_selectors_size,N_st,i_H_psi_array)
+  call i_h_psi_minilist(det_pert,minilist,idx_minilist,N_minilist,psi_selectors_coef,Nint,N_minilist,psi_selectors_size,N_st,i_H_psi_array)
   
   
   h = diag_H_mat_elem_fock(det_ref,det_pert,fock_diag_tmp,Nint)
@@ -34,11 +34,11 @@ subroutine pt2_epstein_nesbet ($arguments)
       e_2_pert(i) = selection_criterion*selection_criterion_factor*2.d0
     else if  (dabs(electronic_energy(i) - h) > 1.d-6) then
         c_pert(i) = i_H_psi_array(i) / (electronic_energy(i) - h)
-        H_pert_diag(i) = h*c_pert(i)*c_pert(i)
-        e_2_pert(i) = c_pert(i) * i_H_psi_array(i)
+        H_pert_diag(i) = h*cdabs(c_pert(i)*conjg(c_pert(i)))
+        e_2_pert(i) = cdabs(conjg(c_pert(i)) * i_H_psi_array(i))
     else
       c_pert(i) = -1.d0
-      e_2_pert(i) = -dabs(i_H_psi_array(i))
+      e_2_pert(i) = -cdabs(i_H_psi_array(i))
       H_pert_diag(i) = h
     endif
   enddo
@@ -55,13 +55,13 @@ subroutine pt2_qdpt ($arguments)
   !
   ! for the various N_st states.
   !
-  ! c_pert(i) = <psi(i)|H|det_pert>/( <psi(i)|H|psi(i)> - <det_pert|H|det_pert> )
+  ! c_pert(i) = <det_pert|H|psi(i)>/( <psi(i)|H|psi(i)> - <det_pert|H|det_pert> )
   !
   END_DOC
   
   integer                        :: i,j
-  double precision               :: diag_H_mat_elem_fock, h, E, diag_H_mat_elem, hij
-  double precision               :: i_H_psi_array(N_st)
+  double precision               :: diag_H_mat_elem_fock, h, E, diag_H_mat_elem
+  complex*16               :: i_H_psi_array(N_st), hij
   integer :: degree
   double precision :: delta_E
   PROVIDE  selection_criterion
@@ -83,7 +83,7 @@ subroutine pt2_qdpt ($arguments)
     endif
     delta_E = E-h
 !    delta_E = electronic_energy(1) - h
-    call i_H_j(psi_selectors(1,1,j),det_pert,Nint,hij)
+    call i_H_j(det_pert,psi_selectors(1,1,j),Nint,hij)
     if (dabs(delta_e) > 1.d-3) then
         do i =1,N_st
           c_pert(i) += psi_selectors_coef(j,i) * hij / delta_e
@@ -91,8 +91,8 @@ subroutine pt2_qdpt ($arguments)
     endif
   enddo
   do i =1,N_st
-    e_2_pert(i) = c_pert(i)*i_H_psi_array(i)
-    H_pert_diag(i) = h*c_pert(i)*c_pert(i)
+    e_2_pert(i) = cdabs(conjg(c_pert(i))*i_H_psi_array(i))
+    H_pert_diag(i) = h*cdabs(conjg(c_pert(i))*c_pert(i))
   enddo
   
 end
@@ -108,19 +108,19 @@ subroutine pt2_decontracted ($arguments)
   
   integer                        :: i,j
   double precision               :: diag_H_mat_elem_fock, h
-  double precision               :: i_H_psi_array(N_st)
-  double precision               :: coef_pert
+  complex*16               :: i_H_psi_array(N_st)
+  complex*16               :: coef_pert
   PROVIDE  selection_criterion
 
   ASSERT (Nint == N_int)
   ASSERT (Nint > 0)
   !call i_H_psi(det_pert,psi_selectors,psi_selectors_coef,Nint,N_det_selectors,psi_selectors_size,N_st,i_H_psi_array)
-  call i_H_psi_pert_new_minilist(det_pert,minilist,idx_minilist,N_minilist,psi_selectors_coef,Nint,N_minilist,psi_selectors_size,N_st,i_H_psi_array,coef_pert)
+  call i_h_psi_pert_new_minilist(det_pert,minilist,idx_minilist,N_minilist,psi_selectors_coef,Nint,N_minilist,psi_selectors_size,N_st,i_H_psi_array,coef_pert)
   H_pert_diag = 0.d0
   
   
   c_pert(1) = coef_pert
-  e_2_pert(1) = coef_pert * i_H_psi_array(1)
+  e_2_pert(1) = cdabs(conjg(coef_pert) * i_H_psi_array(1))
 ! print*,coef_pert,i_H_psi_array(1)
   
 end
@@ -146,7 +146,7 @@ subroutine pt2_epstein_nesbet_2x2 ($arguments)
   
   integer                        :: i,j
   double precision               :: diag_H_mat_elem_fock,delta_e, h
-  double precision               :: i_H_psi_array(N_st)
+  complex*16               :: i_H_psi_array(N_st)
   ASSERT (Nint == N_int)
   ASSERT (Nint > 0)
 
@@ -158,16 +158,16 @@ subroutine pt2_epstein_nesbet_2x2 ($arguments)
     if (i_H_psi_array(i) /= 0.d0) then
       delta_e = h - electronic_energy(i)
       if (delta_e > 0.d0) then
-        e_2_pert(i) = 0.5d0 * (delta_e - dsqrt(delta_e * delta_e + 4.d0 * i_H_psi_array(i) * i_H_psi_array(i)))
+        e_2_pert(i) = 0.5d0 * (delta_e - dsqrt(delta_e * delta_e + 4.d0 * cdabs(conjg(i_H_psi_array(i)) * i_H_psi_array(i))))
       else
-        e_2_pert(i) = 0.5d0 * (delta_e + dsqrt(delta_e * delta_e + 4.d0 * i_H_psi_array(i) * i_H_psi_array(i)))
+        e_2_pert(i) = 0.5d0 * (delta_e + dsqrt(delta_e * delta_e + 4.d0 * cdabs(conjg(i_H_psi_array(i)) * i_H_psi_array(i))))
       endif
-      if (dabs(i_H_psi_array(i)) > 1.d-6) then
-        c_pert(i) = e_2_pert(i)/i_H_psi_array(i)
+      if (cdabs(i_H_psi_array(i)) > 1.d-6) then
+        c_pert(i) = e_2_pert(i)/conjg(i_H_psi_array(i))
       else
         c_pert(i) = 0.d0
       endif
-      H_pert_diag(i) = h*c_pert(i)*c_pert(i)
+      H_pert_diag(i) = h*cdabs(conjg(c_pert(i))*c_pert(i))
 !     print*, 'N_det,N_det_selectors = ',N_det,N_det_selectors
 !     print*, 'threshold_selectors',threshold_selectors
 !     print*, delta_e,i_H_psi_array(1)
@@ -214,7 +214,7 @@ subroutine pt2_epstein_nesbet_2x2_no_ci_diag($arguments)
   
   integer                        :: i,j
   double precision               :: diag_H_mat_elem_fock,delta_e, h
-  double precision               :: i_H_psi_array(N_st)
+  complex*16               :: i_H_psi_array(N_st)
   ASSERT (Nint == N_int)
   ASSERT (Nint > 0)
   PROVIDE psi_energy
@@ -227,16 +227,16 @@ subroutine pt2_epstein_nesbet_2x2_no_ci_diag($arguments)
     if (i_H_psi_array(i) /= 0.d0) then
       delta_e = h - psi_energy(i)
       if (delta_e > 0.d0) then
-        e_2_pert(i) = 0.5d0 * (delta_e - dsqrt(delta_e * delta_e + 4.d0 * i_H_psi_array(i) * i_H_psi_array(i)))
+        e_2_pert(i) = 0.5d0 * (delta_e - dsqrt(delta_e * delta_e + 4.d0 * cdabs(conjg(i_H_psi_array(i)) * i_H_psi_array(i))))
       else
-        e_2_pert(i) = 0.5d0 * (delta_e + dsqrt(delta_e * delta_e + 4.d0 * i_H_psi_array(i) * i_H_psi_array(i)))
+        e_2_pert(i) = 0.5d0 * (delta_e + dsqrt(delta_e * delta_e + 4.d0 * cdabs(conjg(i_H_psi_array(i)) * i_H_psi_array(i))))
       endif
-      if (dabs(i_H_psi_array(i)) > 1.d-6) then
-        c_pert(i) = e_2_pert(i)/i_H_psi_array(i)
+      if (cdabs(i_H_psi_array(i)) > 1.d-6) then
+        c_pert(i) = e_2_pert(i)/conjg(i_H_psi_array(i))
       else
         c_pert(i) = 0.d0
       endif
-      H_pert_diag(i) = h*c_pert(i)*c_pert(i)
+      H_pert_diag(i) = h*cdabs(conjg(c_pert(i))*c_pert(i))
     else
       e_2_pert(i) = 0.d0
       c_pert(i) = 0.d0
@@ -269,7 +269,7 @@ subroutine pt2_moller_plesset ($arguments)
   integer                        :: exc(0:2,2,2)
   integer                        :: degree
   double precision               :: phase,delta_e,h
-  double precision               :: i_H_psi_array(N_st)
+  complex*16               :: i_H_psi_array(N_st)
   integer                        :: h1,h2,p1,p2,s1,s2
   ASSERT (Nint == N_int)
   ASSERT (Nint > 0)
@@ -296,7 +296,7 @@ subroutine pt2_moller_plesset ($arguments)
   do i =1,N_st
     H_pert_diag(i) = h
     c_pert(i) = i_H_psi_array(i) *delta_e
-    e_2_pert(i) = c_pert(i) * i_H_psi_array(i)
+    e_2_pert(i) = cdabs(conjg(c_pert(i)) * i_H_psi_array(i))
   enddo
   
 end
@@ -322,7 +322,7 @@ subroutine pt2_moller_plesset_general ($arguments)
   integer                        :: exc(0:2,2,2)
   integer                        :: degree
   double precision               :: phase,delta_e,h
-  double precision               :: i_H_psi_array(N_st)
+  complex*16               :: i_H_psi_array(N_st)
   integer                        :: h1,h2,p1,p2,s1,s2
   ASSERT (Nint == N_int)
   ASSERT (Nint > 0)
@@ -349,7 +349,7 @@ subroutine pt2_moller_plesset_general ($arguments)
   do i =1,N_st
     H_pert_diag(i) = h
     c_pert(i) = i_H_psi_array(i) *delta_e
-    e_2_pert(i) = c_pert(i) * i_H_psi_array(i)
+    e_2_pert(i) = cdabs(conjg(c_pert(i)) * i_H_psi_array(i))
   enddo
   
 end
@@ -384,7 +384,7 @@ subroutine pt2_epstein_nesbet_SC2_projected ($arguments)
   ! H_pert_diag = <HF|H|det_pert> c_pert
   END_DOC
   
-  double precision               :: i_H_psi_array(N_st)
+  complex*16               :: i_H_psi_array(N_st)
   integer                        :: idx_repeat(0:ndet)
   integer                        :: i,j,degree,l
   double precision               :: diag_H_mat_elem_fock,accu_e_corr,hij,h0j,h,delta_E
@@ -393,10 +393,11 @@ subroutine pt2_epstein_nesbet_SC2_projected ($arguments)
   ASSERT (Nint == N_int)
   ASSERT (Nint > 0)
 
-  call i_H_psi_SC2(det_pert,psi_selectors,psi_selectors_coef,Nint,N_det_selectors,psi_selectors_size,N_st,i_H_psi_array,idx_repeat)
+  call i_h_psi_sc2(det_pert,psi_selectors,psi_selectors_coef,Nint,N_det_selectors,psi_selectors_size,N_st,i_H_psi_array,idx_repeat)
   accu_e_corr = 0.d0
   !$IVDEP
   do i = 1, idx_repeat(0)
+    
    accu_e_corr = accu_e_corr + E_corr_per_selectors(idx_repeat(i))
   enddo
   h = diag_H_mat_elem_fock(det_ref,det_pert,fock_diag_tmp,Nint)
@@ -405,16 +406,16 @@ subroutine pt2_epstein_nesbet_SC2_projected ($arguments)
 
 
   c_pert(1) = i_H_psi_array(1) /(electronic_energy(1) - h)
-  e_2_pert(1) = i_H_psi_array(1) * c_pert(1)
+  e_2_pert(1) = cdabs(i_H_psi_array(1) * conjg(c_pert(1)))
 
   do i =2,N_st
     H_pert_diag(i) = h
     if  (dabs(electronic_energy(i) - h) > 1.d-6) then
       c_pert(i) = i_H_psi_array(i) / (-dabs(electronic_energy(i) - h))
-      e_2_pert(i) = (c_pert(i) * i_H_psi_array(i))
+      e_2_pert(i) = cdabs(conjg(c_pert(i)) * i_H_psi_array(i))
     else
       c_pert(i) = i_H_psi_array(i)
-      e_2_pert(i) = -dabs(i_H_psi_array(i))
+      e_2_pert(i) = -cdabs(i_H_psi_array(i))
     endif
   enddo
 
@@ -428,10 +429,10 @@ subroutine pt2_epstein_nesbet_SC2_projected ($arguments)
   if(degree==4)then
   ! <psi|delta_H|psi>
    e_2_pert_fonda = e_2_pert(1) 
-   H_pert_diag(1) = e_2_pert(1) * c_pert(1) * c_pert(1)
+   H_pert_diag(1) = e_2_pert(1) * cdabs(conjg(c_pert(1)) * c_pert(1))
    do i = 1, N_st
     do j = 1, idx_repeat(0)
-     e_2_pert(i) += e_2_pert_fonda * psi_selectors_coef(idx_repeat(j),i) * psi_selectors_coef(idx_repeat(j),i)
+     e_2_pert(i) += e_2_pert_fonda * cdabs(conjg(psi_selectors_coef(idx_repeat(j),i)) * psi_selectors_coef(idx_repeat(j),i))
     enddo
    enddo
   endif
@@ -467,7 +468,7 @@ subroutine pt2_epstein_nesbet_SC2_no_projected ($arguments)
   ! H_pert_diag = <HF|H|det_pert> c_pert
   END_DOC
   
-  double precision               :: i_H_psi_array(N_st)
+  complex*16               :: i_H_psi_array(N_st)
   integer                        :: idx_repeat(0:ndet)
   integer                        :: i,j,degree,l
   double precision               :: diag_H_mat_elem_fock,accu_e_corr,hij,h0j,h,delta_E
@@ -488,16 +489,16 @@ subroutine pt2_epstein_nesbet_SC2_no_projected ($arguments)
 
 
   c_pert(1) = i_H_psi_array(1) /(electronic_energy(1) - h)
-  e_2_pert(1) = i_H_psi_array(1) * c_pert(1)
+  e_2_pert(1) = cdabs(i_H_psi_array(1) * conjg(c_pert(1)))
 
   do i =2,N_st
     H_pert_diag(i) = h
     if  (dabs(electronic_energy(i) - h) > 1.d-6) then
       c_pert(i) = i_H_psi_array(i) / (-dabs(electronic_energy(i) - h))
-      e_2_pert(i) = (c_pert(i) * i_H_psi_array(i))
+      e_2_pert(i) = cdabs(conjg(c_pert(i)) * i_H_psi_array(i))
     else
       c_pert(i) = i_H_psi_array(i)
-      e_2_pert(i) = -dabs(i_H_psi_array(i))
+      e_2_pert(i) = -cdabs(i_H_psi_array(i))
     endif
   enddo
 end
@@ -522,7 +523,7 @@ subroutine pt2_epstein_nesbet_sc2 ($arguments)
   END_DOC
   
   integer                        :: i,j
-  double precision               :: i_H_psi_array(N_st)
+  complex*16               :: i_H_psi_array(N_st)
   double precision               :: diag_H_mat_elem_fock, h
   PROVIDE  selection_criterion
 
@@ -539,11 +540,11 @@ subroutine pt2_epstein_nesbet_sc2 ($arguments)
       e_2_pert(i) = selection_criterion*selection_criterion_factor*2.d0
     else if  (dabs(electronic_energy(i) - h) > 1.d-6) then
         c_pert(i) = i_H_psi_array(i) / (electronic_energy(i) - h)
-        H_pert_diag(i) = h*c_pert(i)*c_pert(i)
-        e_2_pert(i) = c_pert(i) * i_H_psi_array(i)
+        H_pert_diag(i) = h*cdabs(conjg(c_pert(i))*c_pert(i))
+        e_2_pert(i) = cdabs(conjg(c_pert(i)) * i_H_psi_array(i))
     else
       c_pert(i) = -1.d0
-      e_2_pert(i) = -dabs(i_H_psi_array(i))
+      e_2_pert(i) = -cdabs(i_H_psi_array(i))
       H_pert_diag(i) = h
     endif
   enddo
@@ -561,7 +562,7 @@ subroutine pt2_dummy ($arguments)
   
   integer                        :: i,j
   double precision               :: diag_H_mat_elem_fock, h
-  double precision               :: i_H_psi_array(N_st)
+  complex*16               :: i_H_psi_array(N_st)
   PROVIDE  selection_criterion
 
   call i_H_psi_minilist(det_pert,minilist,idx_minilist,N_minilist,psi_selectors_coef,Nint,N_minilist,psi_selectors_size,N_st,i_H_psi_array)
@@ -570,7 +571,7 @@ subroutine pt2_dummy ($arguments)
   do i =1,N_st
     if (i_H_psi_array(i) /= 0.d0) then
      c_pert(i) = i_H_psi_array(i) / (electronic_energy(i) - h)
-     H_pert_diag(i) = h*c_pert(i)*c_pert(i)
+     H_pert_diag(i) = h*cdabs(conjg(c_pert(i))*c_pert(i))
      e_2_pert(i) = 1.d0
     else
       c_pert(i) = 0.d0
@@ -595,7 +596,7 @@ electronic_energy,det_ref,det_pert,fock_diag_tmp,c_pert,e_2_pert,H_pert_diag,Nin
     integer(bit_kind), intent(in)   :: det_pert(Nint,2)
     double precision , intent(in)   :: fock_diag_tmp(2,mo_tot_num+1)
     double precision , intent(in)    :: electronic_energy(N_st)
-    double precision , intent(out)  :: c_pert(N_st)
+    complex*16 , intent(out)  :: c_pert(N_st)
     double precision , intent(out)  :: e_2_pert(N_st)
     double precision, intent(out)   :: H_pert_diag(N_st)
     integer, intent(in)             :: idx_minilist(0:N_det_selectors)
