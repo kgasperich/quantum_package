779adf3f18e83ce26d080bb803b32e0344c394a7
Kevin Gasperich <kgasperich@gmail.com>
2018-07-13 17:17:08 -0500

    don't calculate ao_bi integrals

--- src/Integrals_Bielec/ao_bi_integrals.irp.f
+++ src/Integrals_Bielec/ao_bi_integrals.irp.f
@@ -360,65 +360,6 @@ BEGIN_PROVIDER [ logical, ao_bielec_integrals_in_map ]
       return
   endif
   
-  print*, 'Providing the AO integrals'
-  call wall_time(wall_0)
-  call wall_time(wall_1)
-  call cpu_time(cpu_1)
-
-  integer(ZMQ_PTR) :: zmq_to_qp_run_socket, zmq_socket_pull
-  call new_parallel_job(zmq_to_qp_run_socket,zmq_socket_pull,'ao_integrals')
-
-  character(len=:), allocatable :: task
-  allocate(character(len=ao_num*12) :: task)
-  write(fmt,*) '(', ao_num, '(I5,X,I5,''|''))'
-  do l=1,ao_num
-    write(task,fmt) (i,l, i=1,l)
-    integer, external :: add_task_to_taskserver
-    if (add_task_to_taskserver(zmq_to_qp_run_socket,trim(task)) == -1) then
-      stop 'Unable to add task to server'
-    endif
-  enddo
-  deallocate(task)
-  
-  integer, external :: zmq_set_running
-  if (zmq_set_running(zmq_to_qp_run_socket) == -1) then
-    print *,  irp_here, ': Failed in zmq_set_running'
-  endif
-
-  PROVIDE nproc
-  !$OMP PARALLEL DEFAULT(shared) private(i) num_threads(nproc+1)
-      i = omp_get_thread_num()
-      if (i==0) then
-        call ao_bielec_integrals_in_map_collector(zmq_socket_pull)
-      else
-        call ao_bielec_integrals_in_map_slave_inproc(i)
-      endif
-  !$OMP END PARALLEL
-
-  call end_parallel_job(zmq_to_qp_run_socket, zmq_socket_pull, 'ao_integrals')
-
-
-  print*, 'Sorting the map'
-  call map_sort(ao_integrals_map)
-  call cpu_time(cpu_2)
-  call wall_time(wall_2)
-  integer(map_size_kind)         :: get_ao_map_size, ao_map_size
-  ao_map_size = get_ao_map_size()
-  
-  print*, 'AO integrals provided:'
-  print*, ' Size of AO map :         ', map_mb(ao_integrals_map) ,'MB'
-  print*, ' Number of AO integrals :', ao_map_size
-  print*, ' cpu  time :',cpu_2 - cpu_1, 's'
-  print*, ' wall time :',wall_2 - wall_1, 's  ( x ', (cpu_2-cpu_1)/(wall_2-wall_1+tiny(1.d0)), ' )'
-  
-  ao_bielec_integrals_in_map = .True.
-
-  if (write_ao_integrals.and.mpi_master) then
-    call ezfio_set_work_empty(.False.)
-    call map_save_to_disk(trim(ezfio_filename)//'/work/ao_ints',ao_integrals_map)
-    call ezfio_set_integrals_bielec_disk_access_ao_integrals("Read")
-  endif
-  
 END_PROVIDER
  
 BEGIN_PROVIDER [ double precision, ao_bielec_integral_schwartz,(ao_num,ao_num)  ]
