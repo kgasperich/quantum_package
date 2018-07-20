aa0c994aa34e3859e924aa05792ceece84c4e74a
Kevin Gasperich <kgasperich@gmail.com>
2018-07-20 12:15:53 -0500

    moved some things around for read/write one-elec ints

--- src/Utils/read_write_ints.irp.f
+++ src/Utils/read_write_ints.irp.f
@@ -1,3 +1,30 @@
+ BEGIN_PROVIDER [ logical, read_ao_one_integrals ]
+&BEGIN_PROVIDER [ logical, write_ao_one_integrals ]
+   
+   BEGIN_DOC
+   ! One level of abstraction for disk_access_ao_integrals and disk_access_mo_integrals
+   END_DOC
+   implicit none
+   
+   if (disk_access_ao_one_integrals.EQ.'Read') then
+     read_ao_one_integrals =  .True.
+     write_ao_one_integrals = .False.
+     
+   else if  (disk_access_ao_one_integrals.EQ.'Write') then
+     read_ao_one_integrals = .False.
+     write_ao_one_integrals =  .True.
+     
+   else if (disk_access_ao_one_integrals.EQ.'None') then
+     read_ao_one_integrals = .False.
+     write_ao_one_integrals = .False.
+     
+   else
+     print *, 'bielec_integrals/disk_access_ao_integrals has a wrong type'
+     stop 1
+     
+   endif
+   
+END_PROVIDER
 
 subroutine write_one_e_integrals(filename, A, m, n)
   implicit none
