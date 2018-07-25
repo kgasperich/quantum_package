e7b630de1b0abe429bda1f7f75c8f3898d491642
Kevin Gasperich <kgasperich@gmail.com>
2018-07-25 16:48:28 -0500

    complex ao mono int read

--- plugins/read_integral/read_ao_mono_complex.irp.f
+++ plugins/read_integral/read_ao_mono_complex.irp.f
@@ -2,6 +2,7 @@ program read_integrals
 
   PROVIDE ezfio_filename
   call ezfio_set_integrals_monoelec_disk_access_ao_one_integrals("None")
+  call ezfio_set_utils_disk_access_ao_overlap_integrals("None")
   call run
 end
 
@@ -17,8 +18,9 @@ subroutine run
   complex*16, allocatable :: A(:,:)
 
   allocate (A(ao_num,ao_num))
+
+
   A = 0.d0
-  
   iunit = getunitandopen('kinetic_ao_complex','r')
   do 
     read (iunit,*,end=10) i,j, int_re, int_im
@@ -31,7 +33,19 @@ subroutine run
 
 
   A = 0.d0
-  iunit = getunitandopen('nuclear_ao_complex','r')
+  iunit = getunitandopen('ne_ao_complex','r')
+  do
+    read (iunit,*,end=11) i,j, int_re, int_im
+    A(i,j) = dcmplx(int_re,int_im)
+    A(j,i) = dcmplx(int_re,-int_im)
+  enddo
+  11 continue
+  close(iunit)
+  call write_one_e_integrals_complex('ao_ne_integral', A, size(A,1), size(A,2))
+
+
+  A = 0.d0
+  iunit = getunitandopen('overlap_ao_complex','r')
   do 
     read (iunit,*,end=12) i,j, int_re, int_im
     A(i,j) = dcmplx(int_re,int_im)
@@ -39,12 +53,12 @@ subroutine run
   enddo
   12 continue
   close(iunit)
-  call write_one_e_integrals_complex('ao_ne_integral', A, size(A,1), size(A,2))
+  call write_one_e_integrals_complex('ao_overlap', A, size(A,1), size(A,2))
+
 
   deallocate(A)
 
-  call write_one_e_integrals_complex('ao_pseudo_integral', ao_pseudo_integral,&
-        size(ao_pseudo_integral,1), size(ao_pseudo_integral,2))
-
   call ezfio_set_integrals_monoelec_disk_access_ao_one_integrals("Read")
+  call ezfio_set_utils_disk_access_ao_overlap_integrals("Read")
+
 end
