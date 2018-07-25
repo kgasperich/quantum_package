cede1b0b2ee698302e730f08dd9c7a527a99f704
Kevin Gasperich <kgasperich@gmail.com>
2018-07-25 16:54:12 -0500

    rename ao map

--- plugins/read_integral/read_ao_eri_chunk_complex.irp.f
+++ plugins/read_integral/read_ao_eri_chunk_complex.irp.f
@@ -42,8 +42,8 @@ subroutine run
     buffer_values_im(n_integrals) = int_im
 
     IF ( MOD(n_integrals,n_chunk_integrals).EQ.0) THEN
-          call insert_into_ao_integrals_map_2fold(n_integrals,buffer_re,buffer_values_re)
-          call insert_into_ao_integrals_map_2fold(n_integrals,buffer_im,buffer_values_im)
+          call insert_into_ao_integrals_map(n_integrals,buffer_re,buffer_values_re)
+          call insert_into_ao_integrals_map(n_integrals,buffer_im,buffer_values_im)
           n_integrals = 0; buffer_re(:) = 0; buffer_values_re(:) = 0.d0
           buffer_im(:) = 0; buffer_values_im(:) = 0.d0
     ENDIF
@@ -53,14 +53,14 @@ subroutine run
   
   !Tail loop
   IF (n_integrals.ne.0) THEN
-    call insert_into_ao_integrals_map_2fold(n_integrals,buffer_re,buffer_values_re)
-    call insert_into_ao_integrals_map_2fold(n_integrals,buffer_im,buffer_values_im)
+    call insert_into_ao_integrals_map(n_integrals,buffer_re,buffer_values_re)
+    call insert_into_ao_integrals_map(n_integrals,buffer_im,buffer_values_im)
   END IF
 
-  call map_sort(ao_integrals_map_2fold)
-  call map_unique(ao_integrals_map_2fold)
+  call map_sort(ao_integrals_map)
+  call map_unique(ao_integrals_map)
 
-  call map_save_to_disk(trim(ezfio_filename)//'/work/ao_ints_2fold',ao_integrals_map_2fold)
+  call map_save_to_disk(trim(ezfio_filename)//'/work/ao_ints',ao_integrals_map)
   call ezfio_set_integrals_bielec_disk_access_ao_integrals('Read')
 
 end
