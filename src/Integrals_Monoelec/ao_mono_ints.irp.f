bb3a2ac3039f2b2f2bb22a18161ad41360bd1137
Kevin Gasperich <kgasperich@gmail.com>
2018-07-25 16:32:00 -0500

    cleanup mono ints

--- src/Integrals_Monoelec/ao_mono_ints.irp.f
+++ src/Integrals_Monoelec/ao_mono_ints.irp.f
@@ -1,14 +1,58 @@
+BEGIN_PROVIDER [complex*16, ao_kinetic_integral, (ao_num,ao_num)]
+  implicit none
+  BEGIN_DOC
+  ! array of the priminitve basis kinetic integrals
+  !  \langle \chi_i |\hat{T}| \chi_j \rangle
+  END_DOC
+  
+  if (read_ao_one_integrals) then
+    call read_one_e_integrals_complex('ao_kinetic_integral', ao_kinetic_integral,&
+        size(ao_kinetic_integral,1), size(ao_kinetic_integral,2))
+    print *,  'AO kinetic integrals read from disk'
+  else
+    print *, 'complex AO kinetic integrals must be provided'
+    stop
+  endif
+END_PROVIDER
+
+BEGIN_PROVIDER [ complex*16, ao_nucl_elec_integral, (ao_num,ao_num)]
+   implicit none
+   BEGIN_DOC
+   ! interaction nuclear electron
+   END_DOC
+   
+   if (read_ao_one_integrals) then
+    call read_one_e_integrals_complex('ao_ne_integral', ao_nucl_elec_integral,      &
+            size(ao_nucl_elec_integral,1), size(ao_nucl_elec_integral,2))
+     print *,  'AO N-e integrals read from disk'
+   else
+     print *, 'complex AO N-e integrals must be provided'
+     stop
+   endif
+   
+END_PROVIDER
+
+ BEGIN_PROVIDER [ complex*16, ao_nucl_elec_integral_per_atom, (ao_num,ao_num,nucl_num)]
+ BEGIN_DOC
+! ao_nucl_elec_integral_per_atom(i,j,k) = -<AO(i)|1/|r-Rk|AO(j)> 
+! where Rk is the geometry of the kth atom
+ END_DOC
+  print *, 'ao_nucl_elec_integral_per_atom not implemented for k-points'
+  stop
+END_PROVIDER
+
+
  BEGIN_PROVIDER [ complex*16, ao_mono_elec_integral,(ao_num,ao_num)]
 &BEGIN_PROVIDER [ double precision, ao_mono_elec_integral_diag,(ao_num)]
   implicit none
-  integer :: i,j,n,l
+  integer :: i,j
   BEGIN_DOC
  ! array of the mono electronic hamiltonian on the AOs basis
  ! : sum of the kinetic and nuclear electronic potential 
   END_DOC
   do j = 1, ao_num
    do i = 1, ao_num
-    ao_mono_elec_integral(i,j) = ao_nucl_elec_integral(i,j) + ao_kinetic_integral(i,j) + ao_pseudo_integral(i,j)
+    ao_mono_elec_integral(i,j) = ao_nucl_elec_integral(i,j) + ao_kinetic_integral(i,j)
    enddo
    ao_mono_elec_integral_diag(j) = real(ao_mono_elec_integral(j,j))
   enddo
--- src/Integrals_Monoelec/mo_mono_ints.irp.f
+++ src/Integrals_Monoelec/mo_mono_ints.irp.f
@@ -1,26 +1,89 @@
+BEGIN_PROVIDER [complex*16, mo_kinetic_integral, (mo_tot_num,mo_tot_num)]
+  implicit none
+  BEGIN_DOC
+  !  Kinetic energy integrals in the MO basis
+  END_DOC
+
+  if (read_mo_one_integrals) then
+    call read_one_e_integrals_complex('mo_kinetic_integral', mo_kinetic_integral,&
+        size(mo_kinetic_integral,1), size(mo_kinetic_integral,2))
+    print *,  'MO kinetic integrals read from disk'
+  else
+    call complex_ao_to_mo(                                           &
+        ao_kinetic_integral,                                         &
+        size(ao_kinetic_integral,1),                                 &
+        mo_kinetic_integral,                                         &
+        size(mo_kinetic_integral,1)                                  &
+        )
+  endif
+  if (write_mo_one_integrals) then
+    call write_one_e_integrals_complex('mo_kinetic_integral', mo_kinetic_integral,&
+        size(mo_kinetic_integral,1), size(mo_kinetic_integral,2))
+    print *,  'MO kinetic integrals written to disk'
+  endif
+
+END_PROVIDER
+
+BEGIN_PROVIDER [complex*16, mo_nucl_elec_integral, (mo_tot_num,mo_tot_num)]
+ implicit none
+ BEGIN_DOC
+! interaction nuclear electron on the MO basis
+ END_DOC
+
+  if (read_mo_one_integrals) then
+    call read_one_e_integrals_complex('mo_ne_integral', mo_nucl_elec_integral, &
+        size(mo_nucl_elec_integral,1), size(mo_nucl_elec_integral,2))
+    print *,  'MO N-e integrals read from disk'
+  else
+    call complex_ao_to_mo(                                            &
+        ao_nucl_elec_integral,                               &
+        size(ao_nucl_elec_integral,1),                       &
+        mo_nucl_elec_integral,                                       &
+        size(mo_nucl_elec_integral,1)                                &
+        )
+  endif
+  if (write_mo_one_integrals) then
+    call write_one_e_integrals_complex('mo_ne_integral', mo_nucl_elec_integral, &
+        size(mo_nucl_elec_integral,1), size(mo_nucl_elec_integral,2))
+    print *,  'MO N-e integrals written to disk'
+  endif
+
+END_PROVIDER
+
+
+BEGIN_PROVIDER [complex*16, mo_nucl_elec_integral_per_atom, (mo_tot_num,mo_tot_num,nucl_num)]
+ implicit none
+ BEGIN_DOC
+! mo_nucl_elec_integral_per_atom(i,j,k) = -<MO(i)|1/|r-Rk|MO(j)> 
+! where Rk is the geometry of the kth atom
+ END_DOC
+
+ integer :: k
+ mo_nucl_elec_integral_per_atom = 0.d0
+ do k = 1, nucl_num 
+   call complex_ao_to_mo(                                               &
+       ao_nucl_elec_integral_per_atom(1,1,k),                        &
+       size(ao_nucl_elec_integral_per_atom,1),                       &
+       mo_nucl_elec_integral_per_atom(1,1,k),                        &
+       size(mo_nucl_elec_integral_per_atom,1)                        &
+       )
+ enddo
+
+END_PROVIDER
+
 BEGIN_PROVIDER [ complex*16, mo_mono_elec_integral,(mo_tot_num,mo_tot_num)]
   implicit none
-  integer                        :: i,j,n,l
+  integer                        :: i,j
   BEGIN_DOC
   ! array of the mono electronic hamiltonian on the MOs basis :
   ! sum of the kinetic and nuclear electronic potential (and pseudo potential if needed)
   END_DOC
   print*,'Providing the mono electronic integrals'
 
-  IF (do_pseudo) THEN
-      do j = 1, mo_tot_num
-      do i = 1, mo_tot_num
-          mo_mono_elec_integral(i,j) = mo_nucl_elec_integral(i,j) + mo_kinetic_integral(i,j) & 
-                                       + mo_pseudo_integral(i,j)
-      enddo
-      enddo
+  do j = 1, mo_tot_num
+    do i = 1, mo_tot_num
+      mo_mono_elec_integral(i,j) = mo_nucl_elec_integral(i,j) + mo_kinetic_integral(i,j)
+    enddo
+  enddo
 
-  ELSE
-      do j = 1, mo_tot_num
-      do i = 1, mo_tot_num
-          mo_mono_elec_integral(i,j) = mo_nucl_elec_integral(i,j) + mo_kinetic_integral(i,j)
-      enddo
-      enddo
-
-  END IF
 END_PROVIDER
