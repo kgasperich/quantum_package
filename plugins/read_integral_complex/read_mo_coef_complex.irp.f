873dd28bc85c314906a47c75d5a2aa979e105149
Kevin Gasperich <kgasperich@gmail.com>
2018-07-31 12:24:52 -0500

    added complex read_integral plugin

---
+++ plugins/read_integral_complex/read_mo_coef_complex.irp.f
@@ -0,0 +1,30 @@
+program read_integrals
+
+  PROVIDE ezfio_filename
+!  call ezfio_set_integrals_monoelec_disk_access_ao_one_integrals("None")
+  call run
+end
+
+subroutine run
+  use map_module
+  implicit none
+  
+  integer :: iunit
+  integer :: getunitandopen
+
+  integer ::i,j
+  double precision :: int_re, int_im
+
+  
+  iunit = getunitandopen('mo_coef_complex','r')
+  provide mo_coef
+  do 
+    read (iunit,*,end=10) i,j, int_re, int_im
+    mo_coef(i,j) = dcmplx(int_re,int_im)
+  enddo
+  10 continue
+  close(iunit)
+  mo_label = "None"
+  call save_mos
+
+end
