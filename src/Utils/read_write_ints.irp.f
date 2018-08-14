c2e91859e9bce478492981fb361c3d05a1fd0aaa
Kevin Gasperich <keg56@pitt.edu>
2018-08-14 17:09:34 -0400

    new utility to read/write monoelec structures using kpts

--- src/Utils/read_write_ints.irp.f
+++ src/Utils/read_write_ints.irp.f
@@ -130,3 +130,75 @@ subroutine read_one_e_integrals_complex(filename, A, m, n)
 
 end
 
+subroutine write_one_e_integrals_complex_kpts(filename, A, m, n, p)
+  implicit none
+  BEGIN_DOC
+! Write the 1-electron integrals stored in A(m,n) into file 'filename'
+  END_DOC
+  character(len=*), intent(in)   :: filename
+  integer, intent(in)            :: m,n,p
+  complex*16, intent(in)   :: A(m,n,p)
+  double precision, allocatable :: A_re(:,:,:), A_im(:,:,:)
+
+  integer                        :: i,j,k
+  integer                        :: iunit
+  integer, external              :: getUnitAndOpen
+  character*(256)                :: f
+
+  allocate( A_re(m,n,p), A_im(m,n,p) )
+
+  do k=1,p
+    do j=1,n
+      do i=1,m
+        A_re(i,j,k) = real(A(i,j,k))
+        A_im(i,j,k) = imag(A(i,j,k))
+      enddo
+    enddo
+  enddo
+
+  iunit = getUnitAndOpen( trim(ezfio_work_dir)//trim(filename)//'_real', 'W' )
+  write(iunit) A_re
+  close(iunit)
+
+  iunit = getUnitAndOpen( trim(ezfio_work_dir)//trim(filename)//'_imag', 'W' )
+  write(iunit) A_im
+  close(iunit)
+  deallocate( A_re, A_im )
+end
+
+subroutine read_one_e_integrals_complex_kpts(filename, A, m, n, p)
+  implicit none
+  BEGIN_DOC
+! Read the 1-electron integrals into in A(m,n) from file 'filename'
+  END_DOC
+  character(len=*), intent(in)   :: filename
+  integer, intent(in)            :: m,n,p
+  complex*16, intent(out)  :: A(m,n,p)
+  double precision, allocatable  :: A_re(:,:,:), A_im(:,:,:)
+
+  integer                        :: i,j,k
+  integer                        :: iunit
+  integer, external              :: getUnitAndOpen
+  character*(256)                :: f
+
+  allocate( A_re(m,n,p), A_im(m,n,p) )
+
+  iunit = getUnitAndOpen( trim(ezfio_work_dir)//trim(filename)//'_real', 'R' )
+  read(iunit) A_re
+  close(iunit)
+  
+  iunit = getUnitAndOpen( trim(ezfio_work_dir)//trim(filename)//'_imag', 'R' )
+  read(iunit) A_im
+  close(iunit)
+
+  do k=1,p
+    do j=1,n
+      do i=1,n
+        A(i,j,k) = cmplx(A_re(i,j,k),A_im(i,j,k))
+      enddo
+    enddo
+  enddo
+  deallocate( A_re, A_im )
+
+end
+
