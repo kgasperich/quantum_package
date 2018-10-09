56ecafcbba92c92d7b8ade24c3c8bbf363b741d7
Kevin Gasperich <kgasperich@gmail.com>
2018-10-09 15:57:15 -0400

    df framework

---
+++ src/Integrals_Bielec/df_integrals.irp.f
@@ -0,0 +1,117 @@
+BEGIN_PROVIDER [complex*16, df_integral_array, (ao_tot_num, ao_tot_num, df_tot_num)]
+  implicit none
+  BEGIN_DOC
+  !  for k-points i,j,k, give k-point l for which the total momentum is conserved over k_i* k_j* k_k k_l
+  END_DOC
+  
+  if (read_df_integral_array) then
+    print *, 'reading df_integral_array from disk'
+    call read_df_integral_array_file('df_integral_array',df_integral_array,num_kpts)
+    print *, 'read df_integral_array from disk'
+  else
+    print *, 'df_integral_array must be provided'
+  endif
+
+END_PROVIDER
+
+
+ BEGIN_PROVIDER [ logical, read_df_integral_array ]
+&BEGIN_PROVIDER [ logical, write_df_integral_array ]
+   
+   BEGIN_DOC
+   ! One level of abstraction for disk_access_df_integral_array
+   END_DOC
+   implicit none
+   
+   if (disk_access_df_integral_array.EQ.'Read') then
+     read_df_integral_array =  .True.
+     write_df_integral_array = .False.
+     
+   else if  (disk_access_df_integral_array.EQ.'Write') then
+     read_df_integral_array = .False.
+     write_df_integral_array =  .True.
+     
+   else if (disk_access_df_integral_array.EQ.'None') then
+     read_df_integral_array = .False.
+     write_df_integral_array = .False.
+     
+   else
+     print *, 'utils/disk_access_df_integral_array has a wrong type'
+     stop 1
+     
+   endif
+   
+END_PROVIDER
+
+subroutine write_df_integral_array_file(filename, A, n_ao, n_df)
+  implicit none
+  BEGIN_DOC
+! Write the 1-electron integrals stored in A(m,n) into file 'filename'
+  END_DOC
+  character(len=*), intent(in)   :: filename
+  integer, intent(in)            :: n_ao
+  complex*16, intent(in)         :: A(n_ao,n_ao,n_df)
+  double precision, allocatable  :: A_re(:,:,:), A_im(:,:,:)
+
+  integer                        :: iunit
+  integer, external              :: getUnitAndOpen
+  character*(256)                :: f
+
+  allocate( A_re(n_ao,n_ao,n_df), A_im(n_ao,n_ao,n_df))
+
+  do i=1,n_ao
+    do j=1,n_ao
+      do k=1,n_df
+        A_re(i,j,k) = real(A(i,j,k))
+        A_im(i,j,k) = imag(A(i,j,k))
+      enddo
+    enddo
+  enddo
+
+  iunit = getUnitAndOpen( trim(ezfio_work_dir)//trim(filename)//'_real', 'W' )
+  write(iunit) A_re
+  close(iunit)
+
+  iunit = getUnitAndOpen( trim(ezfio_work_dir)//trim(filename)//'_imag', 'W' )
+  write(iunit) A_im
+  close(iunit)
+
+  deallocate(A_re,A_im)
+end
+
+subroutine read_df_integral_array_file(filename, A, n_k)
+  implicit none
+  BEGIN_DOC
+! Read the 1-electron integrals into in A(m,n) from file 'filename'
+  END_DOC
+  character(len=*), intent(in)   :: filename
+  integer, intent(in)            :: n_k
+  complex*16, intent(out)        :: A(n_k,n_k,n_k)
+  double precision, allocatable  :: A_re(:,:,:), A_im(:,:,:)
+
+  integer                        :: iunit
+  integer, external              :: getUnitAndOpen
+  character*(256)                :: f
+
+  allocate( A_re(n_ao,n_ao,n_df), A_im(n_ao,n_ao,n_df))
+
+  iunit = getUnitAndOpen( trim(ezfio_work_dir)//trim(filename)//'_real', 'R' )
+  read(iunit) A_re
+  close(iunit)
+
+  iunit = getUnitAndOpen( trim(ezfio_work_dir)//trim(filename)//'_imag', 'R' )
+  read(iunit) A_im
+  close(iunit)
+
+  do i=1,n_ao
+    do j=1,n_ao
+      do k=1,n_df
+        A(i,j,k) = cmplx(A_re(i,j,k),A_im(i,j,k))
+      enddo
+    enddo
+  enddo
+  deallocate(A_re,A_im)
+
+end
+
+
