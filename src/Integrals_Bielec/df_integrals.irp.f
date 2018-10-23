2590e1016e739ff37c4770526e342235559c8747
Kevin Gasperich <kgasperich@gmail.com>
2018-10-23 16:26:43 -0400

    fixed df integrals

--- src/Integrals_Bielec/df_integrals.irp.f
+++ src/Integrals_Bielec/df_integrals.irp.f
@@ -1,4 +1,15 @@
-BEGIN_PROVIDER [complex*16, df_integral_array, (ao_tot_num, ao_tot_num, df_tot_num)]
+
+BEGIN_PROVIDER [integer, ao_num_per_kpt ]
+  implicit none
+  ao_num_per_kpt = ao_num / num_kpts
+END_PROVIDER
+
+BEGIN_PROVIDER [integer, num_kpt_pairs ]
+  implicit none
+  num_kpt_pairs = num_kpts * (num_kpts + 1) / 2
+END_PROVIDER
+
+BEGIN_PROVIDER [complex*16, df_integral_array, (num_kpt_pairs, df_tot_num, ao_num_per_kpt, ao_num_per_kpt)]
   implicit none
   BEGIN_DOC
   !  for k-points i,j,k, give k-point l for which the total momentum is conserved over k_i* k_j* k_k k_l
@@ -6,7 +17,7 @@ BEGIN_PROVIDER [complex*16, df_integral_array, (ao_tot_num, ao_tot_num, df_tot_n
   
   if (read_df_integral_array) then
     print *, 'reading df_integral_array from disk'
-    call read_df_integral_array_file('df_integral_array',df_integral_array,num_kpts)
+    call read_df_integral_array_file('df_integral_array',df_integral_array,df_tot_num,ao_num_per_kpt,num_kpt_pairs)
     print *, 'read df_integral_array from disk'
   else
     print *, 'df_integral_array must be provided'
@@ -43,27 +54,31 @@ END_PROVIDER
    
 END_PROVIDER
 
-subroutine write_df_integral_array_file(filename, A, n_ao, n_df)
+subroutine write_df_integral_array_file(filename, A, n_df, n_ao, n_kpt_pairs)
   implicit none
   BEGIN_DOC
 ! Write the 1-electron integrals stored in A(m,n) into file 'filename'
   END_DOC
   character(len=*), intent(in)   :: filename
-  integer, intent(in)            :: n_ao
-  complex*16, intent(in)         :: A(n_ao,n_ao,n_df)
+  integer, intent(in)            :: n_df, n_ao, n_kpt_pairs
+  complex*16, intent(in)         :: A(n_kpt_pairs,n_df,n_ao,n_ao)
   double precision, allocatable  :: A_re(:,:,:), A_im(:,:,:)
 
   integer                        :: iunit
   integer, external              :: getUnitAndOpen
   character*(256)                :: f
+  
+  integer                        :: i,j,k,l
 
-  allocate( A_re(n_ao,n_ao,n_df), A_im(n_ao,n_ao,n_df))
+  allocate( A_re(n_kpt_pairs,n_df,n_ao,n_ao), A_im(n_kpt_pairs,n_df,n_ao,n_ao))
 
-  do i=1,n_ao
-    do j=1,n_ao
-      do k=1,n_df
-        A_re(i,j,k) = real(A(i,j,k))
-        A_im(i,j,k) = imag(A(i,j,k))
+  do i=1,n_kpt_pairs
+    do j=1,n_df
+      do k=1,n_ao
+        do l=1,n_ao
+          A_re(i,j,k,l) = real(A(i,j,k,l))
+          A_im(i,j,k,l) = imag(A(i,j,k,l))
+        enddo
       enddo
     enddo
   enddo
@@ -79,21 +94,23 @@ subroutine write_df_integral_array_file(filename, A, n_ao, n_df)
   deallocate(A_re,A_im)
 end
 
-subroutine read_df_integral_array_file(filename, A, n_k)
+subroutine read_df_integral_array_file(filename, A, n_df, n_ao, n_kpt_pairs)
   implicit none
   BEGIN_DOC
 ! Read the 1-electron integrals into in A(m,n) from file 'filename'
   END_DOC
   character(len=*), intent(in)   :: filename
-  integer, intent(in)            :: n_k
-  complex*16, intent(out)        :: A(n_k,n_k,n_k)
-  double precision, allocatable  :: A_re(:,:,:), A_im(:,:,:)
+  integer, intent(in)            :: n_df, n_ao, n_kpt_pairs
+  complex*16, intent(out)        :: A(n_kpt_pairs,n_df,n_ao,n_ao)
+  double precision, allocatable  :: A_re(:,:,:,:), A_im(:,:,:,:)
 
   integer                        :: iunit
   integer, external              :: getUnitAndOpen
   character*(256)                :: f
+  
+  integer                        :: i,j,k,l
 
-  allocate( A_re(n_ao,n_ao,n_df), A_im(n_ao,n_ao,n_df))
+  allocate( A_re(n_kpt_pairs,n_df,n_ao,n_ao), A_im(n_kpt_pairs,n_df,n_ao,n_ao))
 
   iunit = getUnitAndOpen( trim(ezfio_work_dir)//trim(filename)//'_real', 'R' )
   read(iunit) A_re
@@ -103,10 +120,12 @@ subroutine read_df_integral_array_file(filename, A, n_k)
   read(iunit) A_im
   close(iunit)
 
-  do i=1,n_ao
-    do j=1,n_ao
-      do k=1,n_df
-        A(i,j,k) = cmplx(A_re(i,j,k),A_im(i,j,k))
+  do i=1,n_kpt_pairs
+    do j=1,n_df
+      do k=1,n_ao
+        do l=1,n_ao
+          A(i,j,k,l) = cmplx(A_re(i,j,k,l),A_im(i,j,k,l))
+        enddo
       enddo
     enddo
   enddo
