57f6ac58328e938c308d252c23521809efb6150d
Kevin Gasperich <kgasperich@gmail.com>
2018-11-01 13:50:52 -0500

    write mo coefs

--- plugins/read_integral_complex/create_ezfio_complex.py
+++ plugins/read_integral_complex/create_ezfio_complex.py
@@ -59,5 +59,5 @@ ezfio.set_mo_basis_ao_md5(ao_md5)
 
 ezfio.set_mo_basis_mo_tot_num(mo_tot_num)
 c_mo = [[1 if i==j else 0 for i in range(mo_tot_num)] for j in range(ao_num)]
-#ezfio.set_mo_basis_mo_coef_real([ [0]*mo_tot_num] * ao_num)
+ezfio.set_mo_basis_mo_coef_real([ [0]*mo_tot_num] * ao_num)
 #ezfio.set_mo_basis_mo_coef_real(c_mo)
