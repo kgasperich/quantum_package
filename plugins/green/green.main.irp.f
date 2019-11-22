aea4673b34a8ae852c24d74fd07370238320c562
Kevin Gasperich <kgasperich@gmail.com>
2019-11-22 11:46:46 -0600

    added eigenvalue printing for lanczos
    eigenvalues will now be printed by executable green.main
    added debug printing for lanczos vectors

--- plugins/green/green.main.irp.f
+++ plugins/green/green.main.irp.f
@@ -6,7 +6,8 @@ program green
   read_wf = .True.
   touch read_wf
   print*,'ref_bitmask_energy = ',ref_bitmask_energy
-  call psicoefprinttest
+!  call psicoefprinttest
+  call print_lanczos_eigvals
   call print_spec
 end
 
@@ -15,6 +16,14 @@ subroutine psicoefprinttest
   integer :: i
   TOUCH psi_coef
   print *, 'printing ndet', N_det
+end
+subroutine print_lanczos_eigvals
+  implicit none
+  integer :: i
+  print *, 'printing lanczos eigenvalues'
+  do i=1,n_lanczos_iter
+    print *, i, lanczos_eigvals(i)
+  enddo
 end
 subroutine print_spec
   implicit none
