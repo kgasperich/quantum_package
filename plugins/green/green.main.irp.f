9d6825e6dbf97c08f0a22b191f1b38154aede5ff
Kevin Gasperich <kgasperich@gmail.com>
2019-11-22 12:18:38 -0600

    nicer lanczos printing

--- plugins/green/green.main.irp.f
+++ plugins/green/green.main.irp.f
@@ -19,17 +19,23 @@ subroutine psicoefprinttest
 end
 subroutine print_lanczos_eigvals
   implicit none
-  integer :: i
-  print *, 'printing lanczos eigenvalues'
+  integer :: i, iunit
+  integer :: getunitandopen
+  iunit = getunitandopen('lanczos_eigval_alpha_beta.out','w')
+  print *, 'printing lanczos eigenvalues, alpha, beta to "lanczos_eigval_alpha_beta.out"'
   do i=1,n_lanczos_iter
-    print *, i, lanczos_eigvals(i)
+    write(iunit,'(I6,3(E25.15))') i, lanczos_eigvals(i), alpha_lanczos(i), beta_lanczos(i)
   enddo
+  close(iunit)
 end
 subroutine print_spec
   implicit none
-  integer :: i
-  print *, 'printing spectral density'
+  integer :: i, iunit
+  integer :: getunitandopen
+  iunit = getunitandopen('omega_A.out','w')
+  print *, 'printing frequency, spectral density to "omega_A.out"'
   do i=1,n_omega
-    print *, i, omega_list(i), spectral_lanczos(i)
+    write(iunit,'(2(E25.15))') omega_list(i), spectral_lanczos(i)
   enddo
+  close(iunit)
 end
