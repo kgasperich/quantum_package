27995998bb81accd87f35127c9a6044a4f9f4616
Kevin Gasperich <kgasperich@gmail.com>
2020-01-06 17:11:48 -0600

    testing for only n-1 lanczos

--- plugins/green/green_ref.irp.f
+++ plugins/green/green_ref.irp.f
@@ -5,10 +5,11 @@ program green
   END_DOC
   read_wf = .True.
   touch read_wf
+  provide n_green_vec
 !  print*,'ref_bitmask_energy = ',ref_bitmask_energy
 !  call psicoefprinttest
-  call print_lanczos_eigvals_homo
-  call print_spec_homo
+  call print_lanczos_eigvals
+  call print_spec
 end
 
 subroutine psicoefprinttest
@@ -17,33 +18,33 @@ subroutine psicoefprinttest
   TOUCH psi_coef
   print *, 'printing ndet', N_det
 end
-subroutine print_lanczos_eigvals_homo
+subroutine print_lanczos_eigvals
   implicit none
   integer :: i, iunit, j
   integer :: getunitandopen
   character(5) :: jstr
 
-  do j=0,0
+  do j=1,n_green_vec
     write(jstr,'(I0.3)') j
     iunit = getunitandopen('lanczos_eigval_alpha_beta.out.'//trim(jstr),'w')
     print *, 'printing lanczos eigenvalues, alpha, beta to "lanczos_eigval_alpha_beta.out.'//trim(jstr)//'"'
     do i=1,n_lanczos_iter
-      write(iunit,'(I6,3(E25.15))') i, lanczos_eigvals_homo(i), alpha_lanczos_homo(i), beta_lanczos_homo(i)
+      write(iunit,'(I6,3(E25.15))') i, lanczos_eigvals(i,j), alpha_lanczos(i,j), beta_lanczos(i,j)
     enddo
     close(iunit)
   enddo
 end
-subroutine print_spec_homo
+subroutine print_spec
   implicit none
   integer :: i, iunit, j
   integer :: getunitandopen
   character(5) :: jstr
-  do j=0,0
+  do j=1,n_green_vec
     write(jstr,'(I0.3)') j
     iunit = getunitandopen('omega_A.out.'//trim(jstr),'w')
     print *, 'printing frequency, spectral density to "omega_A.out.'//trim(jstr)//'"'
     do i=1,n_omega
-      write(iunit,'(3(E25.15))') omega_list(i), spectral_lanczos_homo(i)
+      write(iunit,'(2(E25.15))') omega_list(i), spectral_lanczos(i,j)
     enddo
     close(iunit)
   enddo
